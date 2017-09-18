d3566251b9af55e586df0c112297e53ecc8c2e7b
michaelstaahle <michael.staahle@gmail.com>
2017-09-18 17:22:02 +0200

    Added a textfile for answering the questions. Have printed the trees for all data sets and done some error checking.

--- dectrees/python/Assignment1.py
+++ dectrees/python/Assignment1.py
@@ -1,9 +1,21 @@
 import monkdata as m
 import dtree as dtree
+import drawtree_qt5 as dt
 
 monk1 = m.monk1
 monk2 = m.monk2
 monk3 = m.monk3
+
+monk1test = m.monk1test
+monk2test = m.monk2test
+monk3test = m.monk3test
+
+
+mydic = {'a':4,'b':5}
+print(mydic['a'])
+print(mydic)
+#print(monk1.attribute)
+
 
 print(len(monk1))
 
@@ -35,16 +47,22 @@ print(gain_monk1)
 print(gain_monk2)
 print(gain_monk3)
 
-#for monk in range(1,4):
-#	gain_list = []
-#	for attribute in range(6):
-#		gain_list.append(dtree.averageGain('monk{}'.format(monk),m.attributes[attribute])
-#		print(gain_list)
+BestAttribute = dtree.bestAttribute(monk3,m.attributes)
+print(BestAttribute) 
 
+#monk1_A5_1 = dtree.select(monk1,m.attributes[5],1)
+#print(monk1_A5_1)
 
-#for i in range(1, 4):
-#	for j in range(6):
-#		gain[str(i)] = i
-#		print('s')
+monk1_tree = dtree.buildTree(monk1,m.attributes)
+#graf1 = dt.drawTree(monk1_tree)
 
-#print(gain['2'])
+monk2_tree = dtree.buildTree(monk2,m.attributes)
+#graf2 = dt.drawTree(monk2_tree)
+
+monk3_tree = dtree.buildTree(monk3,m.attributes)
+#graf3 = dt.drawTree(monk3_tree)
+
+print(dtree.check(monk1_tree, m.monk1test))
+print(dtree.check(monk2_tree, m.monk2test))
+print(dtree.check(monk3_tree, m.monk3test))
+
